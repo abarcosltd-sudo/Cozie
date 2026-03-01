831e400bf2c91c7364d3c0d244a157a9a0200a8d
Abarcos tech <abarcosltd@gmail.com>
2026-03-01 22:24:17 +0100

    Add ChartItem model with user chart retrieval method

--- models/ChartItem.js
+++ models/ChartItem.js
@@ -1,1 +1,21 @@
+const db = require("../config/firebase");
 
+class ChartItem {
+  static collection() {
+    return db.collection("chartItems");
+  }
+
+  static async getUserCharts(userId, genres) {
+    const snapshot = await this.collection()
+      .where("genreId", "in", genres)
+      .orderBy("number", "asc")
+      .get();
+
+    return snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  }
+}
+
+module.exports = ChartItem;
