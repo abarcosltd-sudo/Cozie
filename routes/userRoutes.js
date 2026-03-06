873bf7bdb6baa0063bc96ad26a929ed815432ddf
Abarcos tech <abarcosltd@gmail.com>
2026-03-06 21:48:52 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 // routes/userRoutes.js
 import express from "express";
-import { signupUser, getProfile, loginUser, verifyOTP } from "../controllers/userController.js";
+import { signupUser, getProfile, loginUser, verifyOTP, savePreferences } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -8,8 +8,10 @@ router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.get("/profile", getProfile);
 router.post("/verify-otp", verifyOTP);
+router.post("/preferences", savePrefences);
 
 export default router;
+
 
 
 
