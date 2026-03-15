f8dbbe7165b06e9cd1d7b59e93487deb428888c0
Abarcos tech <abarcosltd@gmail.com>
2026-03-15 19:17:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import nodemailer from 'nodemailer'; // or use your preferred email service
 
 import userRoutes from "./routes/userRoutes.js";
 import musicRoutes from "./routes/musicRoutes.js";
-import postRoutes from "./routes/postMusicRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/music", musicRoutes);
-app.use("/api/posts", postMusicRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/api/home", (req, res) => {
   res.json({
