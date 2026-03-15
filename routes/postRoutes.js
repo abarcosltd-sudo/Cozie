5df4a722285658096af2e614b58b1c6e360c7a06
Abarcos tech <abarcosltd@gmail.com>
2026-03-15 20:58:56 +0100

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 // routes/postRoutes.js
 import express from "express";
-import { shareMusicPost, getMusicPosts } from "../controllers/musicPostController.js";
+import { shareMusicPost, getMusicPosts, likePost } from "../controllers/musicPostController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/share-music", protect, shareMusicPost);
 router.get("/feed", getMusicPosts);
+router.post('/:postId/like', likePost);
 
 export default router;
