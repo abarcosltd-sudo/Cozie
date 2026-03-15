9e2aa98d14c3a87a8ade9b79da56e9504816d003
Abarcos tech <abarcosltd@gmail.com>
2026-03-15 18:05:51 +0100

    Update musicRoutes.js

--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -1,1 +1,12 @@
+// routes/musicRoutes.js
+import express from "express";
+import { generateUploadURL, generateAlbumArtURL, addMusic } from "../controllers/musicController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
+const router = express.Router();
+
+router.post("/generate-upload-url", generateUploadURL);
+router.post("/generate-album-art-url", generateAlbumArtURL);
+router.post("/add-music", addMusic);
+
+export default router;
