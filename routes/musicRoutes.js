f56811c26c943f6abfb12dcd3a7ebbcd6966b30b
Abarcos tech <abarcosltd@gmail.com>
2026-03-22 14:48:00 +0100

    Update musicRoutes.js

--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -1,6 +1,7 @@
 // routes/musicRoutes.js
 import express from "express";
 import { generateUploadURL, generateAlbumArtURL, addMusic, searchMusic, getTrendingMusic, getTopCharts } from "../controllers/musicController.js";
+import { likeSong, getSongLikes, getUserLikedSongs } from '../controllers/musicLikeController.js';
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -11,5 +12,8 @@ router.post("/add-music", addMusic);
 router.get("/search", searchMusic);
 router.get('/trending', getTrendingMusic);
 router.get('/charts', getTopCharts);
+router.post('/:songId/like', likeSong);
+router.get('/:songId/likes', getSongLikes);
+router.get('/liked', getUserLikedSongs);
 
 export default router;
