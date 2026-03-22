b942097be553a6c941e53f60f40a3a65ebd6969f
Abarcos tech <abarcosltd@gmail.com>
2026-03-22 14:19:32 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 // routes/userRoutes.js
 import express from "express";
 import { signupUser, getProfile, loginUser, verifyOTP, savePreferences, getCurrentUser, updateProfile, generateUploadURL } from "../controllers/userController.js";
+import { checkFavorite, addFavorite, removeFavorite, getFavorites } from '../controllers/favouritesController.js';
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -13,6 +14,10 @@ router.post("/preferences", protect, savePreferences);
 router.get("/me", protect, getCurrentUser);
 router.put("/profile", protect, updateProfile); // multer is inside the controller
 router.post("/generate-upload-url", protect, generateUploadURL)
+router.get('/favorites', protect, getFavorites);
+router.get('/favorites/:songId', protect, checkFavorite);
+router.post('/favorites/:songId', protect, addFavorite);
+router.delete('/favorites/:songId', protect, removeFavorite);
 
 export default router;
 
