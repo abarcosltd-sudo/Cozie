934296dd547a597e078b2d9a9ae5375edd9a5373
Abarcos tech <abarcosltd@gmail.com>
2026-03-22 16:28:47 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 // routes/userRoutes.js
 import express from "express";
-import { signupUser, getProfile, loginUser, verifyOTP, savePreferences, getCurrentUser, updateProfile, generateUploadURL } from "../controllers/userController.js";
+import { signupUser, getProfile, loginUser, verifyOTP, savePreferences, getCurrentUser, updateProfile, generateUploadURL, getAvailableUsers } from "../controllers/userController.js";
 import { checkFavorite, addFavorite, removeFavorite, getFavorites } from '../controllers/favouritesController.js';
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -10,9 +10,10 @@ router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.get("/profile", getProfile);
 router.post("/verify-otp", verifyOTP);
+router.get('/available', protect, getAvailableUsers);
 router.post("/preferences", protect, savePreferences);
 router.get("/me", protect, getCurrentUser);
-router.put("/profile", protect, updateProfile); // multer is inside the controller
+router.put("/profile", protect, updateProfile); 
 router.post("/generate-upload-url", protect, generateUploadURL)
 router.get('/favorites', protect, getFavorites);
 router.get('/favorites/:songId', protect, checkFavorite);
