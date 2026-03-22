b36ad43cf50977bdff87b00282d7b8f72ac96e4a
Abarcos tech <abarcosltd@gmail.com>
2026-03-22 16:35:48 +0100

    Create messageRoutes.js

---
+++ routes/messageRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  getAvailableUsers,
+  getConversations,
+  getMessages,
+  sendMessage,
+  deleteMessage
+} from '../controllers/messagesController.js';
+
+const router = express.Router();
+
+// Conversations
+router.get('/', getConversations);
+router.get('/:conversationId', getMessages);
+router.post('/:userId', sendMessage);
+router.delete('/:messageId', deleteMessage);
+
+export default router;
