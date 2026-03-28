e74c716e76a7142ee7e62401a3547f061e99acb6
Abarcos tech <abarcosltd@gmail.com>
2026-03-28 23:27:24 +0100

    Update musicRoutes.js

--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -12,9 +12,10 @@ router.post("/add-music", protect, addMusic);
 router.get("/search", protect, searchMusic);
 router.get('/trending', protect, getTrendingMusic);
 router.get('/charts', protect, getTopCharts);
+router.get('/:songId', protect, getSongById);
 router.post('/:songId/like', protect, likeSong);
 router.get('/:songId/likes', protect, getSongLikes);
 router.get('/liked', protect, getUserLikedSongs);
-router.get('/:songId', protect, getSongById);
+
 
 export default router;
