f146f89d37d927c57b9735e2ebb89c8e60f4b9ca
Abarcos tech <abarcosltd@gmail.com>
2026-03-28 23:17:24 +0100

    Update musicRoutes.js

--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -1,6 +1,6 @@
 // routes/musicRoutes.js
 import express from "express";
-import { generateUploadURL, generateAlbumArtURL, addMusic, searchMusic, getTrendingMusic, getTopCharts } from "../controllers/musicController.js";
+import { generateUploadURL, generateAlbumArtURL, addMusic, searchMusic, getTrendingMusic, getTopCharts, getSongById } from "../controllers/musicController.js";
 import { likeSong, getSongLikes, getUserLikedSongs } from '../controllers/musicLikeController.js';
 import { protect } from "../middleware/authMiddleware.js";
 
